57c34694e6a52c67bf7824bdc85d6a1456dfbb4e
ranxx <ranxx@chinawyny.com>
2020-08-22 11:35:51 +0800

    less

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import a from "./mo";
-import tsFun from "./ts.ts";
+import tsFun from "./ts/ts.ts";
+
+import "./test";
+import styles from "./less/style.less";
+
 require("./style.css");
 document.write("it works");
 document.write(require("./bar.js"));
 document.write(a.a);
 document.write(tsFun("input"));
+
+// less
+//document.write(<div className={styles.test}>哈哈哈哈</div>);
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,10 @@
+/*
+ * @Autor: jarze
+ * @Date: 2020-08-08 17:11:32
+ * @Desc: config
+ */
 import path from "path";
+import process from "process";
 import { Configuration } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
@@ -6,11 +12,27 @@ const config: Configuration = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].[hash].js",
   },
+  //devtool: "inline-source-map",
   module: {
     rules: [
       { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader",
+        // use: [
+        //   {
+        //     loader: "style-loader",
+        //   },
+        //   {
+        //     loader: "css-loader",
+        //   },
+        //   {
+        //     loader: "less-loader",
+        //   },
+        // ],
+      },
       { test: /\.(js|jsx)$/, loader: "babel-loader" },
       { test: /\.(ts|tsx)$/, loader: "ts-loader" },
     ],
@@ -25,10 +47,41 @@ const config: Configuration = {
       //   collapseWhitespace: true, // 删除空白符与换行符
       //   minifyCSS: true, // 压缩内联css
       // },
-      // filename: "index.html",
-      // template: "index.html",
+      filename: "index.html",
+      template: "./public/index.html",
     }),
   ],
+  devtool: process.env.mode === "development" ? "source-map" : false,
+  // externals: {
+  //   jquery: "jQuery",
+  // },
+
+  // 性能提示文件大小超过5000bytes警告⚠️
+  performance: {
+    hints: "warning",
+    // 入口起点的最大体积
+    maxEntrypointSize: 10000,
+    // 单个资源体积
+    maxAssetSize: 5000,
+    // 只给出 .js 文件的性能提示
+    assetFilter: function (assetFilename) {
+      return assetFilename.endsWith(".js");
+    },
+  },
+
+  node: {},
 };
 
-export default config;
+//webpack-dev-server
+const devServer = {
+  port: 8888,
+  proxy: {
+    "/api": {
+      target: "http://localhost:3000",
+      pathRewrite: { "^/api": "" },
+    },
+  },
+  // progress:
+};
+
+export default { devServer, ...config };
